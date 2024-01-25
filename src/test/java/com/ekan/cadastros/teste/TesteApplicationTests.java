939198ea6a65a9cc4a634d784b6bd081bb091b29
Victor Philipe Arakaki <philipesan123@hotmail.com>
2024-01-25 15:58:06 -0300

    feature: Commit inicial do projeto

---
+++ src/test/java/com/ekan/cadastros/teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekan.cadastros.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
